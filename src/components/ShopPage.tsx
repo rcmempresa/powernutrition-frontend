a1fefb54fa13c06507235600edb91547e9f9670a
rcmempresa <rcmiranda.empresa@gmail.com>
2025-09-06 14:26:55 +0100

    mais alteracoes

--- src/components/ShopPage.tsx
+++ src/components/ShopPage.tsx
@@ -412,8 +412,6 @@ const ShopPage: React.FC<ShopPageProps> = ({
   };
 });
 
-console.log(processedProducts);
-
   processedProducts.forEach(product => {
     if (filterType === 'availability') {
       const key = product.isAvailable ? 'Em stock' : 'Fora de stock';
