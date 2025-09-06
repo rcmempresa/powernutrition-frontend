9807216a5e894d7cc1db17b63ed28193bb029c89
rcmempresa <rcmiranda.empresa@gmail.com>
2025-09-06 13:39:05 +0100

    mais alteracoes

--- src/components/ShopPage.tsx
+++ src/components/ShopPage.tsx
@@ -975,14 +975,10 @@ const ShopPage: React.FC<ShopPageProps> = ({
                         <p className="text-xs text-gray-400 mb-2">{product.brand_name}</p> 
                     )}
                     <div className="flex items-baseline mb-2">
-                      {product.original_price && parseFloat(product.displayPrice) < parseFloat(product.original_price) && (
-                        <p className="text-gray-500 line-through text-base md:text-lg">
-                          €{parseFloat(product.original_price).toFixed(2)}
-                        </p>
+                      <span className="text-xl font-bold text-orange-500 mr-2">€{product.price.toFixed(2)}</span>
+                      {product.original_price && product.original_price > product.price && (
+                          <span className="text-gray-500 line-through">€{product.original_price.toFixed(2)}</span>
                       )}
-                      <p className="text-red-500 font-bold text-lg md:text-xl">
-                        € {product.displayPrice.toFixed(2)}
-                      </p>
                     </div>
                     {product.rating !== undefined && (
                       <div className="flex items-center text-yellow-500">
