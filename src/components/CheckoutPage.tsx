075f3ff5946bf1192eb8a82eeb6a37cbe10022bd
rcmempresa <rcmiranda.empresa@gmail.com>
2025-09-16 13:15:40 +0100

    mais alteracoes

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -140,7 +140,7 @@ const CheckoutPage: React.FC<CheckoutPageProps> = ({ items, onBack }) => {
           price: item.price,
           quantity: item.quantity,
           original_price: item.original_price, 
-          product_id: item.product_id, ß
+          product_id: item.product_id, 
         })),
       };
 
